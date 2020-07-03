21661270644ed539e948ce423fb05303d5cb5e1c
KLisabeth <professional.translations4ppl@gmail.com>
2020-07-03 10:29:49 +0200

    build

---
+++ week-1-project/build/precache-manifest.504cafa4ace1df93ac31a31dea631255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cc1ba86cc1e17750bf0d36ecd6a4f7",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/index.html"
+  },
+  {
+    "revision": "eab2181de029d235b562",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.5670d731.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.5670d731.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cd6cd538d9cdb8d35f6",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/main.b0f4c5dd.chunk.js"
+  },
+  {
+    "revision": "a10d73b1a54dfcae7f1a",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/runtime-main.e77b2ca0.js"
+  }
+]);
